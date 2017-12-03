be860ee5fee378be9904b8d3b9cb6b725d10f7c2
snehathadani <snehacopper@gmail.com>
2017-12-02 19:18:45 -0800

    updated and corrected errors

--- server.js
+++ server.js
@@ -13,8 +13,57 @@ const STATUS_USER_ERROR = 422;
 
 server.use(bodyParser.json());
 
-// Your API will be built out here.
+// Your API will be built out here. write a 'get' request to '/users' that simply returns all the peopel.
 
+server.get('/users', (req, res)=> {
+Person.find({}, (err, users) =>{
+  if (err) {
+    res.status(STATUS_SERVER_ERROR).json({'Error getting your users' : error});
+    return;
+  }
+  res.json(users);
+});
+});
+server.get('/users/:direction', (req, res)=> {
+  const { direction } = req.params;// asc or desc
+  Person.find({})
+  .sort({'firstName':direction})
+  .exec((err, sortedUsers)=> {
+  if (err) {
+    res.status(STATUS_USER_ERROR).json({'Error getting/ sorting your users: ' :err});
+    return;
+  }
+  res.json(sortedUsers);
+  });
+});
+
+server.get('/user-get-friends/:id', (req, res)=>{
+const { id } = req.params;
+Person.findById(id)
+  .select('friends')
+  .exec((err, friends)=> {
+    if (err) {
+      res.status(422).json({ 'Could not find any user by that data' : err});
+      return;
+    }
+    res.json(friends);
+
+  });
+});
+server.put ('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName } = req.body;
+  console.log (req.body);
+  Person.findByIdAndUpdate(id, {firstName, lastName})
+  .exec((err, updatedUser) => {
+    if (err) {
+      res.status(422).json ({'Could not find user by that id: ' : err});
+      return;
+    }
+    res.json(updatedUser);
+  
+  });
+});
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect('mongodb://localhost/people', {
   useMongoClient: true
